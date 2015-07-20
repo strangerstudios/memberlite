10f17cdda69b8fd6d137a669510b91501bf9ca4d
Kimberly Coleman <kim@strangerstudios.com>
2015-07-20 10:29:00 -0400

    Added nesting to col and row shortcodes; styling for pmpro_content_message

--- inc/shortcodes/columns.php
+++ inc/shortcodes/columns.php
@@ -2,12 +2,10 @@
 /* Adapted from https://github.com/wp-plugins/easy-foundation-shortcodes/blob/master/shortcode/wpcolumns/plugin_shortcode.php */
 
 // Rows
-
 function memberlite_row_shortcode($atts, $content = null) {
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
 	// examples: [row class="examples"]
-
     extract(shortcode_atts(array(
         'class' => ''
     ), $atts));
@@ -16,22 +14,20 @@ function memberlite_row_shortcode($atts, $content = null) {
     $content = str_replace("<br />\n[", '[', $content);
     $result .= do_shortcode($content);
     $result .= '</div>';
-
     return force_balance_tags($result);
 }
-
 add_shortcode('row', 'memberlite_row_shortcode');
+add_shortcode('row_row', 'memberlite_row_shortcode');
+add_shortcode('row_row_row', 'memberlite_row_shortcode');
+add_shortcode('row_row_row_your_boat', 'memberlite_row_shortcode');
 
-/* * *********************************************************
- * TWO
- * ********************************************************* */
-
+//Columns
 function memberlite_col_shortcode($atts, $content = null) {
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
 	// examples: [col large="3" medium="6" large_offset="3"]
-
     extract(shortcode_atts(array(
+		'class' => '',
 		'small' => '',
 		'medium' => '',
 		'large' => '',
@@ -39,19 +35,18 @@ function memberlite_col_shortcode($atts, $content = null) {
 		'medium_offset' => '',
 		'large_offset' => '',
     ), $atts));
-
     $arr = array('small'=>'small','medium'=>'medium','large'=>'large','small-offset' =>'small_offset','medium-offset' =>'medium_offset','large-offset' =>'large_offset');
     $classes = array();
     foreach ($arr as $k => $aa) {
 		if(!empty(${$aa}))
 			$classes[] = $k.'-' . ${$aa};
     }
-
-    $result = '<div class="' . implode(' ', $classes) . ' columns">';
+    $result = '<div class="' . implode(' ', $classes) . ' ' . $class . ' columns">';
     $result .= do_shortcode($content);
     $result .= '</div>';
-
     return force_balance_tags($result);
 }
-
 add_shortcode('col', 'memberlite_col_shortcode');
+add_shortcode('col_col', 'memberlite_col_shortcode');
+add_shortcode('col_col_col', 'memberlite_col_shortcode');
+add_shortcode('col_col_col_me_maybe', 'memberlite_col_shortcode');
