96951db34af6fa3f9e1dcff986b4342d28c99a02
Kimberly Coleman <kim@strangerstudios.com>
2015-08-01 12:41:31 -0400

    added icon attribute to memberlite_btn shortcode

--- inc/shortcodes/buttons.php
+++ inc/shortcodes/buttons.php
@@ -2,10 +2,11 @@
 function memberlite_btn_shortcode($atts, $content = null) {
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
-	// examples: [memberlite_btn style="success" href="http://www.paidmembershipspro.com" text="Paid Memberships Pro"]
+	// examples: [memberlite_btn style="success" href="http://www.paidmembershipspro.com" text="Paid Memberships Pro" icon="info"]
     extract(shortcode_atts(array(
 		'style' => 'default',
 		'href' => '#',
+		'icon' => '',
 		'target' => 'self',
 		'text' => 'Go to Link'
     ), $atts));
@@ -14,6 +15,8 @@ function memberlite_btn_shortcode($atts, $content = null) {
 		$r = '<a class="btn_link" href="' . $href . '" target="_' . $target . '">';
 	else
 		$r = '<a class="btn btn_' . $style . '" href="' . $href . '" target="_' . $target . '">';
+	if(!empty($icon))
+		$r .= '<i class="fa fa-' . $icon . '"></i>';	
     $r .= $text;
     $r .= '</a>';
     return $r;
