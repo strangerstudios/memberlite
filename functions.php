e909b69dcd88c4dafb85466024f0dbbeed5d65d2
Jason Coleman <jason@strangerstudios.com>
2016-02-17 14:37:30 -0500

    moving styles from shortcodes plugin back to theme

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @package Memberlite
  */
-define('MEMBERLITE_VERSION', '2.0.3.6');
+define('MEMBERLITE_VERSION', '3.0');
 
 //get default values for options/etc
 require_once get_template_directory() . '/inc/defaults.php';
