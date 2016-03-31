663eed3e1784e1feb5bb3c002e67a8e111f2d68b
Jason Coleman <jason@strangerstudios.com>
2016-03-31 12:04:16 -0400

    hiding page nav links on PMPro membership account and subpages

---
+++ inc/integrations/paid-memberships-pro.php
@@ -0,0 +1,26 @@
+<?php
+/*
+	Don't show the page nav on the PMPro Membership Account page and subpages.
+*/
+function memberlite_pmpro_theme_mod_memberlite_page_nav($mod) {
+	global $pmpro_pages, $post;
+		
+	//no post or pmpro_pages and we stop
+	if(empty($pmpro_pages) || empty($pmpro_pages['account']) || empty($post) || empty($post->ID))
+		return $mod;
+		
+	//which page to be checking
+	if(!empty($post->post_parent))
+	{
+		$post_ancestors = get_post_ancestors($post);
+		$toplevelpost = end($post_ancestors);
+	}
+	else
+		$toplevelpost = $post->ID;
+	
+	if($toplevelpost == $pmpro_pages['account'])
+		$mod = 0;
+	
+	return $mod;
+}
+add_filter('theme_mod_memberlite_page_nav', 'memberlite_pmpro_theme_mod_memberlite_page_nav');
