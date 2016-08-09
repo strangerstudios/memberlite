327a0134acb50dd2d6ed94be8923688f3c606c3a
Jason Coleman <jason@strangerstudios.com>
2016-08-09 11:52:51 -0400

    fixing strict standards notice

--- content-page.php
+++ content-page.php
@@ -20,7 +20,8 @@
 		?>
 		
 		<?php 
-			if( !empty( get_theme_mod( 'memberlite_page_nav', 1 ) ) && !is_page_template('templates/fluid-width.php') )
+			$memberlite_page_nav = get_theme_mod( 'memberlite_page_nav', 1 );
+			if( !empty( $memberlite_page_nav ) && !is_page_template('templates/fluid-width.php') )
 				memberlite_page_nav();
 		?>
 		
