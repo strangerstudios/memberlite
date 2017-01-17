ec3d73b524a2387c89d69453b71a8ba37d632877
greathmaster <counthmaster@yahoo.com>
2017-01-16 18:14:25 -0800

    ENHANCEMENT: Added comment support to pages

--- templates/interstitial.php
+++ templates/interstitial.php
@@ -8,7 +8,13 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<?php do_action('before_loop'); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content-interstitial', 'page' ); ?>				
+				<?php get_template_part( 'content-interstitial', 'page' ); ?>
+				<?php
+				//If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
+				endif;
+				?>
 			<?php endwhile; // end of the loop. ?>
 			<?php do_action('after_loop'); ?>
 		</main><!-- #main -->
