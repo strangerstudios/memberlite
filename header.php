024ec6e5c8a474e201864d26453c500eebf3435d
Jason Coleman <jason@strangerstudios.com>
2017-02-14 16:25:31 -0500

    Pingback only added if singular and pings open.

--- header.php
+++ header.php
@@ -12,7 +12,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php if ( is_singular() && pings_open() ) { ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php } ?>
 
 <?php wp_head(); ?>
 </head>
