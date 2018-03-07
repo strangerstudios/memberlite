21b9c6974f9deecb7224ecfee09e5b20d90a06b2
Jason Coleman <jason@strangerstudios.com>
2018-03-07 13:15:59 -0500

    Merged 3 js files into one for frontend.

--- js/memberlite.js
+++ js/memberlite.js
@@ -46,4 +46,47 @@ jQuery( document ).ready( function() {
 	//check if we should switch tab content on page loads
 	jQuery('a[href="' + window.location.hash + '"]').click(); 
 
+	//mobile navigation
+	var mobilenav_trigger = jQuery('button.menu-toggle');
+	jQuery('#mobile-navigation').after(jQuery('<div id="mobile-navigation-height-col"></div>'));	
+	mobilenav_trigger.click(function() {	
+			
+		jQuery('#mobile-navigation').toggleClass('toggled');
+		
+		if(jQuery('#mobile-navigation').hasClass('toggled')){
+			jQuery('#mobile-navigation').animate({
+				left: '0px'
+			});
+			jQuery('#mobile-navigation-height-col').animate({
+				left: '0px'
+			});	
+		} else {			
+			jQuery('#mobile-navigation').animate({
+				left: '-100%'
+			});
+			jQuery('#mobile-navigation-height-col').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	//skip link focus fix
+	//borrowed from _s theme: https://git.io/vWdr2
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
+		window.addEventListener( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+
 });
