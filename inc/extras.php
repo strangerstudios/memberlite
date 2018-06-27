1c8a32d73f6332b367ccbdd3979c980ad9da569e
Jason Coleman <jason@strangerstudios.com>
2018-06-27 18:11:05 -0400

    Removed back compat code to set author data.

--- inc/extras.php
+++ inc/extras.php
@@ -93,27 +93,6 @@ function memberlite_getColumnsRatio( $location = null ) {
 }
 
 /**
- * Sets the authordata global when viewing an author archive.
- *
- * This provides backwards compatibility with
- * http://core.trac.wordpress.org/changeset/25574
- *
- * It removes the need to call the_post() and rewind_posts() in an author
- * template to print information about the author.
- *
- * @global WP_Query $wp_query WordPress Query object.
- * @return void
- */
-function memberlite_setup_author() {
-	global $wp_query;
-
-	if ( $wp_query->is_author() && isset( $wp_query->post ) ) {
-		$GLOBALS['authordata'] = get_userdata( $wp_query->post->post_author );
-	}
-}
-add_action( 'wp', 'memberlite_setup_author' );
-
-/**
  * Get the width of a thumbnail.
  */
 function memberlite_getPostThumbnailWidth( $post_id = null ) {
