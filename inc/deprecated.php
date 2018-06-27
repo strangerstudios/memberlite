444b06a75d1c64a2008b5639a7ac7b28eb88ee2b
Jason Coleman <jason@strangerstudios.com>
2018-06-27 18:05:58 -0400

    revamped to use the correct anonmyous function syntax based on php version

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -44,16 +44,21 @@ $memberlite_map_deprecated_hooks = array(
 );
 
 // anonymous function used below is only supported in php 5.3+
-if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
-	foreach ( $memberlite_map_deprecated_hooks as $new => $old ) {
-		// assumes hooks with no parameters
-		add_action(
-			$new, function() use ( $new, $old ) {
-				if ( has_filter( $old ) ) {
-					trigger_error( sprintf( __( 'The %1$s hook has been deprecated since version 3.1 of Memberlite. Please use the %2$s hook instead.' ), $old, $new ) );
-					do_action( $old );
-				}
-			}
-		);
+foreach ( $memberlite_map_deprecated_hooks as $new => $old ) {
+	// assumes hooks with no parameters
+	if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
+		// Using anonmyous functions for PHP 5.3+
+		$func = function() use ( $new, $old ) { memberlite_maybe_show_deprecated_hook_message( $new, $old ); };
+	} else {
+		// Using create_function for PHP 5.2
+		$func = create_function( '', "memberlite_maybe_show_deprecated_hook_message( '$new', '$old' );" );
+	}
+	add_action( $new, $func );
+}
+
+function memberlite_maybe_show_deprecated_hook_message( $new, $old ) {
+	if ( has_filter( $old ) ) {
+		trigger_error( sprintf( __( 'The %1$s hook has been deprecated since version 3.1 of Memberlite. Please use the %2$s hook instead.' ), $old, $new ) );
+		do_action( $old );
 	}
 }
