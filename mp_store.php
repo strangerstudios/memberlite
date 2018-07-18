f505369dd9e8d53db80142a9e61137bcadc4554e
Jason Coleman <jason@strangerstudios.com>
2018-07-18 10:40:25 -0400

    Updated header comment to mention MarketPress template.

--- mp_store.php
+++ mp_store.php
@@ -1,11 +1,7 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Template for the MarketPress ecommerce plugin.
+ * https://wordpress.org/plugins/wordpress-ecommerce/
  *
  * @package Memberlite
  */
