60dc62451cbfab75ad7fda7e14d5976e941903f7
Kimberly Coleman <kim@strangerstudios.com>
2018-10-04 21:01:28 -0400

    code formating

--- search.php
+++ search.php
@@ -10,10 +10,8 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 		<?php do_action( 'memberlite_before_loop' ); ?>
 		<?php if ( have_posts() ) : ?>
-			<?php
-			while ( have_posts() ) :
-				the_post();
-?>
+			<?php while ( have_posts() ) :
+				the_post(); ?>
 				<?php get_template_part( 'components/post/content', get_post_format() ); ?>
 			<?php endwhile; ?>
 			<?php memberlite_paging_nav(); ?>
