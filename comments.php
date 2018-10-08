b413b0631cdd7fd79878013118ca5c425a8c7527
Kimberly Coleman <kim@strangerstudios.com>
2018-10-08 16:13:09 -0400

    Fixing missing end break for if statement

--- comments.php
+++ comments.php
@@ -68,7 +68,8 @@ if ( post_password_required() ) {
 				// If comments are closed and there are comments, let's leave a little note, shall we?
 				if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) { ?>
 					<p class="pmpro_message pmpro_alert no-comments"><?php esc_html_e( 'Comments are closed.', 'memberlite' ); ?></p>
-				<?php } else { ?>
+				<?php }
+			} else { ?>
 				<div class="memberlite_tabbable">
 					<ul class="memberlite_tabs">
 						<?php if ( count( $memberlite_comments ) > 0 ) { ?>
