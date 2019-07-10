c66d15b84dfb5e20712d2b47721dcbe6f8c88f44
Theuns Coetzee <ipokkel@gmail.com>
2019-07-10 07:16:05 +0200

    Add polyfill for closest

--- js/memberlite.js
+++ js/memberlite.js
@@ -12,6 +12,24 @@ TODO: Transpile (Babel)
 
 // *** Polyfills ***
 
+// Closest
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                              Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 // Before
 // https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/before#polyfill 
 (function (arr) {
