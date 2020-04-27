a9a21c8cbf1726cfc5b7b60b7e3ec66ef16a4f3a
Kim Coleman <kim@strangerstudios.com>
2020-04-27 14:49:48 -0400

    Adding filter for memberlite_page_title on the login page for Paid Memberships Pro.

--- inc/integrations/paid-memberships-pro.php
+++ inc/integrations/paid-memberships-pro.php
@@ -25,3 +25,34 @@ function memberlite_pmpro_theme_mod_memberlite_page_nav( $mod ) {
 	return $mod;
 }
 add_filter( 'theme_mod_memberlite_page_nav', 'memberlite_pmpro_theme_mod_memberlite_page_nav' );
+
+/**
+ * Filter the page title on the Log In page based on page action.
+ *
+ */
+function pmpro_login_memberlite_page_title( $page_title_html ) {
+	global $pmpro_pages;
+
+	if ( ! is_main_query() ) {
+		return $page_title_html;
+	}
+
+	if ( empty( $pmpro_pages ) || ! is_page( $pmpro_pages['login'] ) ) {
+		return $page_title_html;
+	}
+
+	if ( is_user_logged_in() ) {
+		$title = __( 'Welcome', 'paid-memberships-pro' );
+	} elseif ( ! empty( $_REQUEST['action'] ) && $_REQUEST['action'] === 'reset_pass' ) {
+		$title = __( 'Lost Password', 'paid-memberships-pro' );
+	} elseif ( ! empty( $_REQUEST['action'] ) && $_REQUEST['action'] === 'rp' ) {
+		$title = __( 'Reset Password', 'paid-memberships-pro' );
+	} else {
+		return $page_title_html;
+	}
+
+	$page_title_html = '<h1 class="page-title">' . esc_html__( $title ) . '</h1>';
+
+	return $page_title_html;
+}
+add_filter( 'memberlite_page_title', 'pmpro_login_memberlite_page_title' );
