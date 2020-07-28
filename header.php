f2b5b317a7c499f62ef9c8d481d96ce24a6bb9ba
Kim Coleman <kim@strangerstudios.com>
2020-07-28 15:35:49 -0400

    Adding new filter to allow custom code or child theme to completely hide the header right area in logo space.

--- header.php
+++ header.php
@@ -33,9 +33,17 @@
 		<div class="row">
 			<?php
 				$meta_login = get_theme_mod( 'meta_login', false );
-			if ( ! is_page_template( 'templates/interstitial.php' ) && ( ! empty( $meta_login ) || has_nav_menu( 'meta' ) || is_active_sidebar( 'sidebar-3' ) ) ) {
-				$show_header_right = true;
-			}
+				if ( ! is_page_template( 'templates/interstitial.php' ) && ( ! empty( $meta_login ) || has_nav_menu( 'meta' ) || is_active_sidebar( 'sidebar-3' ) ) ) {
+					$show_header_right = true;
+				}
+				/**
+				 * Filter to hide or show the right column area of the header.
+				 *
+				 * @param bool $show_header_right True to show the header right, false to hide it.
+				 *
+				 * @return bool $show_header_right
+				 */
+				$show_header_right = apply_filters( 'memberlite_show_header_right', $show_header_right );
 			?>
 
 			<div class="
