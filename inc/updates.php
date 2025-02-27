15fb6f97dbaa20f60fc8ba471b5262ddfc71edbb
Andrew Lima <andrew@arctek.co.za>
2025-02-27 12:06:53 +0200

    Add support for updates
    * ENHANCEMENT: Added logic for supporting multiple themes in the JSON and to update memberlite specifically from the JSON update information.

--- inc/updates.php
+++ inc/updates.php
@@ -11,8 +11,8 @@ function memberlite_checkForUpdates() {
 
 	// default DB version for Memberlite 4.0
 	if( empty( $memberlite_db_version ) ) {
-		$memberlite_db_version = '2018080101';
-		update_option('memberlite_db_version', $memberlite_db_version, 'no');
+		$memberlite_db_version = '2025022701'; // Example of 2025-02-27{01} release.
+		update_option( 'memberlite_db_version', $memberlite_db_version, 'no' );
 	}
 }
 
@@ -22,38 +22,34 @@ function memberlite_checkForUpdates() {
  * @since 2.0
 */
 function memberlite_setup_update_info() {
-	if ( ! defined( 'PMPRO_LICENSE_SERVER' ) )
-		define('PMPRO_LICENSE_SERVER', 'https://license.paidmembershipspro.com');
+	if ( ! defined( 'PMPRO_LICENSE_SERVER' ) ) {
+		define('PMPRO_LICENSE_SERVER', 'https://license.paidmembershipspro.com/v2/' );
+	}
 
 	add_filter( 'pre_set_site_transient_update_themes', 'memberlite_update_themes_filter' );
 	add_filter( 'http_request_args', 'memberlite_http_request_args_for_update_info', 10, 2 );
 	add_action( 'update_option_pmpro_license_key', 'memberlite_update_option_pmpro_license_key', 10, 2 );
 }
-add_action( 'init', 'memberlite_setup_update_info' );
+add_action( 'admin_init', 'memberlite_setup_update_info' );
 
 /**
  * Get theme update information from the PMPro server.
- *
- * @since  2.0
+ * @since  TBD
  */
 function memberlite_get_update_info() {
 	// Check if forcing a pull from the server.
 	$update_info = get_option( 'memberlite_update_info', false );
 	$update_info_timestamp = get_option( 'memberlite_update_info_timestamp', 0 );
 
-	// If no update_infos locally, we need to hit the server.
+	// Query the server if we do not have the local $update_info or we force checking for an update.
 	if ( empty( $update_info ) || ! empty( $_REQUEST['force-check'] ) || current_time('timestamp') > $update_info_timestamp + 86400 ) {
-		var_dump( $update_info );
 		/**
-         * Filter to change the timeout for this wp_remote_get() request.
-         *
-         * @since 2.0.1
-         *
+         * Filter to change the timeout for this wp_remote_get() request for updates.
+         * @since TBD
          * @param int $timeout The number of seconds before the request times out
          */
         $timeout = apply_filters( 'memberlite_get_update_info_timeout', 5 );
-		//get em
-		$remote_info = wp_remote_get( PMPRO_LICENSE_SERVER . 'themes/memberlite', $timeout );
+		$remote_info = wp_remote_get( PMPRO_LICENSE_SERVER . 'themes.json', $timeout );
 
 		// Test response.
         if ( is_wp_error( $remote_info ) || empty( $remote_info['response'] ) || $remote_info['response']['code'] != '200' ) {
@@ -76,9 +72,7 @@ function memberlite_get_update_info() {
 
 /**
 * Infuse theme update details when WordPress runs its update checker.
-*
-* @since 2.0
-*
+* @since TBD
 * @param object $value  The WordPress update object.
 * @return object $value Amended WordPress update object on success, default if object is empty.
 */
@@ -89,20 +83,30 @@ function memberlite_update_themes_filter( $value ) {
 		return $value;
 	}
 
-	// get update_info information
+	// Get the update JSON for Stranger Studios themes
 	$update_info = memberlite_get_update_info();
-
-	// no info?
-	if(empty($update_info))
+	// No info found, let's bail.
+	if ( empty( $update_info ) ) {
 		return $value;
+	}
+
+	// Find the memberlite theme data in the update info array.
+	$find_theme = array_search( 'memberlite', array_column( $update_info, 'Slug' ) );
+	
+	// If the theme update data is found, adjust $update_info to be specifically for memberlite.
+	if ( $find_theme !== false ) {
+		$update_info = $update_info[$find_theme];
+	} else {
+		return $value;
+	}
 
 	//get data for theme
 	$theme_file_abs = ABSPATH . 'wp-content/themes/' . $update_info['Slug'];
 	$theme_file = $theme_file_abs . "/style.css";
-	$theme_data = wp_get_theme($update_info['Slug']);
+	$theme_data = wp_get_theme( $update_info['Slug'] );
+	
 	//compare versions
-	if(!empty($update_info['License']) && version_compare($theme_data['Version'], $update_info['Version'], '<'))
-	{
+	if ( ! empty( $update_info['License'] ) && version_compare( $theme_data['Version'], $update_info['Version'], '<' ) ){
 		$value->response[$update_info['Slug']] = array(
 			'theme' => $update_info['Slug'],
 			'new_version' => $update_info['Version'],
@@ -118,7 +122,7 @@ function memberlite_update_themes_filter( $value ) {
 /**
  * Disables SSL verification to prevent download package failures.
  *
- * @since 2.0
+ * @since TBD
  *
  * @param array $args  Array of request args.
  * @param string $url  The URL to be pinged.
@@ -126,8 +130,9 @@ function memberlite_update_themes_filter( $value ) {
  */
 function memberlite_http_request_args_for_update_info($args, $url)  {
 	// If this is an SSL request and we are performing an upgrade routine, disable SSL verification.
-	if(strpos($url, 'https://') !== false && strpos($url, PMPRO_LICENSE_SERVER) !== false && strpos($url, "download") !== false)
+	if ( strpos( $url, 'https://' ) !== false && strpos( $url, PMPRO_LICENSE_SERVER ) !== false && strpos( $url, "download") !== false ) {
 		$args['sslverify'] = false;
+	}
 
 	return $args;
 }
@@ -142,6 +147,6 @@ function memberlite_http_request_args_for_update_info($args, $url)  {
  * @return array $args Amended array of request args.
  */
 function memberlite_update_option_pmpro_license_key( $old_value, $value ) {
-	delete_option('memberlite_update_info_timestamp');
-	delete_site_transient('update_themes');
+	delete_option( 'memberlite_update_info_timestamp' );
+	delete_site_transient( 'update_themes' );
 }
