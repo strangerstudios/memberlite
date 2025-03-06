4ed28b6498efa9def2790b14f2f26993386a82cc
Kim Coleman <kim@strangerstudios.com>
2025-03-06 05:18:31 -0500

    WPCS

--- functions.php
+++ functions.php
@@ -492,19 +492,19 @@ function memberlite_check_for_updated_translations() {
 	// If the library isn't loaded, bail.
 	if ( ! function_exists( 'Memberlite\Required\Traduttore_Registry\add_project' ) ) {
 		return;
-	}     
+	}
 	
 	// Only check for updates when on the update page, plugins, themes page, or the Memberlite support page
-    $is_update_or_plugins_page = strpos( $_SERVER['REQUEST_URI'], 'update-core.php' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'plugins.php' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'themes.php' ) !== false;
-    $is_memberlite_admin_page = isset( $_REQUEST['page'] ) && $_REQUEST['page'] === 'memberlite-support';
+	$is_update_or_plugins_page = strpos( $_SERVER['REQUEST_URI'], 'update-core.php' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'plugins.php' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'themes.php' ) !== false;
+	$is_memberlite_admin_page = isset( $_REQUEST['page'] ) && $_REQUEST['page'] === 'memberlite-support';
 
-    if ( $is_memberlite_admin_page || $is_update_or_plugins_page ) {
-        Memberlite\Required\Traduttore_Registry\add_project(
-            'theme',
-            'memberlite',
-            'https://translate.strangerstudios.com/api/translations/memberlite'
-        );
-    }
+	if ( $is_memberlite_admin_page || $is_update_or_plugins_page ) {
+		Memberlite\Required\Traduttore_Registry\add_project(
+			'theme',
+			'memberlite',
+			'https://translate.strangerstudios.com/api/translations/memberlite'
+		);
+	}
 }
 add_action( 'admin_init', 'memberlite_check_for_updated_translations' );
 
