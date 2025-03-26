70131ed5f57bc3ba05fbff49408a97020f968aed
Kim Coleman <kim@strangerstudios.com>
2025-03-26 07:02:34 -0400

    Adding upgrade script

--- inc/updates.php
+++ inc/updates.php
@@ -10,8 +10,24 @@ function memberlite_checkForUpdates() {
 	$memberlite_db_version = get_option('memberlite_db_version', 0);
 	
 	// default DB version for Memberlite 4.0
-	if( empty( $memberlite_db_version ) ) {
+	if ( empty( $memberlite_db_version ) ) {
 		$memberlite_db_version = '2018080101';
 		update_option('memberlite_db_version', $memberlite_db_version, 'no');
+	}
+
+	// Migrate the theme_mod for webfonts to single properties.
+	// Update the database version to 2025032601
+	if ( $memberlite_db_version < '2025032601' ) {
+		$memberlite_webfonts = get_theme_mod( 'memberlite_webfonts' );
+		if ( ! empty( $memberlite_webfonts ) ) {
+			// Break the theme mod for custom fonts into two parts.
+			$fonts_string_parts = explode( '_', $memberlite_webfonts );
+			$memberlite_header_font = $fonts_string_parts[0];
+			$memberlite_body_font = $fonts_string_parts[1];
+			set_theme_mod( 'memberlite_header_font', $memberlite_header_font );
+			set_theme_mod( 'memberlite_body_font', $memberlite_body_font );
+			remove_theme_mod( 'memberlite_webfonts' );
+		}
+		update_option( 'memberlite_db_version', '2025032601', 'no' );
 	}
 }
