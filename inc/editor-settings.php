231845037d500cf55ad34a82041bb585b28bb84a
unknown <rachelrvasquez@gmail.com>
2026-02-12 21:47:45 -0500

    removed checkbox in the metabox we'll be deprecating soon, and officially registered the existing post meta because the REST API needs to read it (the block editor)

--- inc/editor-settings.php
+++ inc/editor-settings.php
@@ -35,6 +35,16 @@ function memberlite_register_editor_settings_post_meta() : void {
 			return current_user_can( 'edit_posts' );
 		}
 	) );
+
+	register_post_meta( 'page', '_memberlite_hide_page_nav', array(
+		'show_in_rest' => true,
+		'single'       => true,
+		'type'         => 'boolean',
+		'default'      => false,
+		'auth_callback' => function() {
+			return current_user_can( 'edit_posts' );
+		}
+	) );
 }
 add_action( 'init', 'memberlite_register_editor_settings_post_meta' );
 
