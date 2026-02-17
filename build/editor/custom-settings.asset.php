7298e20df8cf50d8b4d0ad9942ef8dd0d224320c
Kim Coleman <kim@strangerstudios.com>
2026-02-16 20:28:20 -0500

    Adding build files so Memberlite 7.0 branch can be used for testing

---
+++ build/editor/custom-settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => 'a794d287d697d3aca028');
