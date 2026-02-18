ac27fb0e5a41b35ad6d00a40a0dc830346ce5569
unknown <rachelrvasquez@gmail.com>
2026-02-18 15:52:01 -0500

    mobile header re-design done for the moment

--- components/header/header-mobile-menu.php
+++ components/header/header-mobile-menu.php
@@ -8,32 +8,35 @@
 <?php do_action( 'memberlite_before_mobile_nav' ); ?>
 
 <?php
-	// show the mobile menu widget area
-	if ( is_active_sidebar( 'sidebar-5' ) || has_nav_menu( 'primary' ) ) { ?>
-		<nav id="mobile-navigation" role="navigation">
+// show the mobile menu widget area
+if ( is_active_sidebar( 'sidebar-5' ) || has_nav_menu( 'primary' ) ) { ?>
+	<div id="mobile-navigation">
+		<div class="mobile-navigation-bar">
 			<button id="close-mobile-nav" class="menu-toggle" aria-controls="mobile-navigation" aria-expanded="true">
+
 				<i class="fa fa-times" aria-hidden="true"></i>
 				<span class="screen-reader-text">
 					<?php _e( 'Close mobile menu', 'memberlite' ); ?>
 				</span>
 			</button>
+		</div>
 		<?php
-			if ( is_active_sidebar( 'sidebar-5' ) ) {
-				dynamic_sidebar( 'sidebar-5' );
-			} elseif ( has_nav_menu( 'primary' ) ) {
-				if ( ! empty( memberlite_is_meta_login_active() ) ) {
-					get_template_part( 'components/header/header', 'member-info' );
-				}
-				wp_nav_menu(
-					array(
-						'theme_location' => 'primary',
-					)
-				);
+		if ( is_active_sidebar( 'sidebar-5' ) ) {
+			dynamic_sidebar( 'sidebar-5' );
+		} elseif ( has_nav_menu( 'primary' ) ) {
+			if ( ! empty( memberlite_is_meta_login_active() ) ) {
+				get_template_part( 'components/header/header', 'member-info' );
 			}
+			wp_nav_menu(
+				array(
+					'theme_location' => 'primary',
+				)
+			);
+		}
 		?>
-		</nav>
-		<?php
-	}
+	</div>
+	<?php
+}
 ?>
 
 <?php do_action( 'memberlite_after_mobile_nav' ); ?>
