b9556c55a50b25d2fe187ea93299585b7f502581
Kim Coleman <kim@strangerstudios.com>
2026-02-23 05:26:32 -0500

    Removing legacy PHP version check with version bump

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -44,19 +44,11 @@ $memberlite_map_deprecated_hooks = array(
 	'memberlite_editor_color_palette'	=> null,
 );
 
-// anonymous function used below is only supported in php 5.3+
 foreach ( $memberlite_map_deprecated_hooks as $new => $old ) {
 	// assumes hooks with no parameters
-	if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
-		// Using anonmyous functions for PHP 5.3+
-		$func = function() use ( $new, $old ) {
-			memberlite_maybe_show_deprecated_hook_message( $new, $old );
-		};
-	} else {
-		// Using create_function for PHP 5.2
-		$func = create_function( '', "memberlite_maybe_show_deprecated_hook_message( '$new', '$old' );" );
-	}
-	add_action( $new, $func );
+	add_action( $new, function() use ( $new, $old ) {
+		memberlite_maybe_show_deprecated_hook_message( $new, $old );
+	} );
 }
 
 function memberlite_maybe_show_deprecated_hook_message( $new, $old ) {
