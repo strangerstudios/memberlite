ca2ff7950baba51983d1e3d9556b05845849bf57
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-03-02 09:07:58 -0500

    testing automated RTL solution

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
  * @package Memberlite
  */
 ?><!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> dir="rtl">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
