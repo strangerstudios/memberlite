8426f24527ca52b3301e46dda0f0e2181d4b4a30
RachieVee <RachelRVasquez@users.noreply.github.com>
2026-03-05 02:58:15 -0500

    Automating RTL language styles from our existing styles (#221)
    * testing automated RTL solution
    
    * make sure font awesome chevron gets flipped when rtl compiles via the config/CSS comment, update navigation functions to flip previous/next markup and arrows
    
    * remove old rtl css
    
    * updated the build process docs to include info about the RTL compiling
    
    * config for RTLCSS
    
    * remove playwright, don't need to commit it
    
    * doc corrections, tweak to config file
    
    * adjust conditional logic in prev/next post link function
    
    * Updating repo files
    
    ---------
    
    Co-authored-by: Kim Coleman <kim@strangerstudios.com>

--- functions.php
+++ functions.php
@@ -10,14 +10,12 @@ define( 'MEMBERLITE_DIR', get_template_directory() );
 
 // enqueue additional stylesheets and javascript
 function memberlite_init_styles() {
-	global $memberlite_defaults;
-
-	// framework stuff
 	wp_enqueue_style( 'memberlite_style', get_stylesheet_uri(), array(), MEMBERLITE_VERSION );
-	wp_enqueue_style( 'memberlite_main_style', MEMBERLITE_URL . '/build/css/main.css', array(), MEMBERLITE_VERSION );
 
 	if ( is_rtl() ) {
-		wp_enqueue_style( 'memberlite_rtl', MEMBERLITE_URL . '/css/rtl.css', array( 'memberlite_style' ), MEMBERLITE_VERSION );
+		wp_enqueue_style( 'memberlite_rtl', MEMBERLITE_URL . '/build/css/main.rtl.css', array( 'memberlite_style' ), MEMBERLITE_VERSION );
+	} else {
+		wp_enqueue_style( 'memberlite_main_style', MEMBERLITE_URL . '/build/css/main.css', array(), MEMBERLITE_VERSION );
 	}
 	wp_enqueue_style( 'memberlite_print_style', MEMBERLITE_URL . '/css/print.css', array(), MEMBERLITE_VERSION, 'print' );
 	wp_enqueue_script( 'memberlite-script', MEMBERLITE_URL . '/js/memberlite.js', array( 'jquery' ), MEMBERLITE_VERSION, true );
