3bf3baec8ff6bbbf8643e7be7757cbff7a72eca3
Kim Coleman <kim@strangerstudios.com>
2026-03-05 11:37:32 -0500

    Adding build files for pull to live site

--- build/editor/custom-settings.asset.php
+++ build/editor/custom-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => 'a794d287d697d3aca028');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '2c7496f2965314cc9aeb');
