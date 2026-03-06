0ad9e56881fedb6aecf58efb940741df079b47bd
Kim Coleman <kim@strangerstudios.com>
2026-03-05 19:36:10 -0500

    Final updates for the push live on PMPro site

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @package Memberlite
  */
-define( 'MEMBERLITE_VERSION', '6.1.1.1.5' );
+define( 'MEMBERLITE_VERSION', '6.1.1.1.10' );
 define( 'MEMBERLITE_URL', get_template_directory_uri() );
 define( 'MEMBERLITE_DIR', get_template_directory() );
 
