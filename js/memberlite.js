14d3d5d61ee8ee3039d9956ee811ea7e1b94a00d
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-03-10 13:59:46 -0400

    update memberlite version in style.css, remove stray console log

--- js/memberlite.js
+++ js/memberlite.js
@@ -76,7 +76,6 @@ function initDesktopNav() {
 	}
 
 	if (memberNav) {
-		console.log('TEST');
 		memberNav.querySelectorAll('.menu-item-has-children').forEach(initDesktopNavItem);
 		initDesktopNavClickOutside(memberNav);
 	}
