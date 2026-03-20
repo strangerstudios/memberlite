fb1547be5aeb525f4894c6041d12733a6fb5e092
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-03-20 14:51:53 -0400

    pattern variation 01

---
+++ patterns/footer-01.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Title: Footer Variation 1
+ * Slug: memberlite/footer-01
+ * Description: Memberlite footer with two rows, 4 columns on top of 2.
+ * Categories: memberlite-footer, footer
+ * Keywords: footer, variation
+ * @package WordPress
+ * @subpackage Memberlite
+ * @since Memberlite 7.0
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|page-masthead"}}}},"backgroundColor":"color-primary","textColor":"page-masthead","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-page-masthead-color has-color-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%"} -->
+		<div class="wp-block-column" style="flex-basis:33%"><!-- wp:site-logo /-->
+
+			<!-- wp:site-title {"fontSize":"18"} /-->
+
+			<!-- wp:paragraph {"fontSize":"14"} -->
+			<p class="has-14-font-size">Zazu SA (Pty) Ltd.<br>Darter Studios, Darter Road<br>Gardens, Cape Town, 800 – South Africa</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group"><!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} -->
+					<p style="font-style:normal;font-weight:500">Consulting</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:paragraph -->
+					<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+					<!-- /wp:paragraph --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} -->
+					<p style="font-style:normal;font-weight:500">Project Management</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:paragraph -->
+					<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+					<!-- /wp:paragraph --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} -->
+					<p style="font-style:normal;font-weight:500">Project Management</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:paragraph -->
+					<p>Our vision is to be at the forefront of architectural innovation, fostering a global community of architects and enthusiasts united by a passion for creating spaces.</p>
+					<!-- /wp:paragraph --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%"} -->
+		<div class="wp-block-column" style="flex-basis:33%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.75rem"}},"textColor":"white"} -->
+			<p class="has-white-color has-text-color" style="font-size:0.75rem">Banking services provided by a South African bank and regulated by the South African Reserve Bank.</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+		<div class="wp-block-column"><!-- wp:paragraph {"className":"has-text-align-right","style":{"typography":{"fontSize":"0.75rem"}},"textColor":"white"} -->
+			<p class="has-text-align-right has-white-color has-text-color" style="font-size:0.75rem">© 2026 Memberlite. All Rights Reserved.</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
