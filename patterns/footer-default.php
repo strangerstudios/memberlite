5cb8ba40cd285d77af1a3d487594f772aa9fa563
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-03-20 15:41:39 -0400

    tweaks to default footer pattern

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -10,56 +10,56 @@
  * @since TBD
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group footer-variation-default"><!-- wp:columns {"align":"full","style":{"border":{"top":{"color":"var:preset|color|borders","width":"1px"},"bottom":{"color":"var:preset|color|borders","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"},"blockGap":{"top":"0","left":"var:preset|spacing|50"}}},"backgroundColor":"footer-widgets-background","textColor":"footer-widgets","fontSize":"14"} -->
-<div class="wp-block-columns alignfull has-footer-widgets-color has-footer-widgets-background-background-color has-text-color has-background has-14-font-size" style="border-top-color:var(--wp--preset--color--borders);border-top-width:1px;border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
-<!-- /wp:heading -->
+<!-- wp:group {"align":"full","className":"footer-variation-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"0","left":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|page-masthead"}}},"border":{"top":{"color":"var:preset|color|borders","width":"1px"}}},"backgroundColor":"color-primary","textColor":"page-masthead","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull footer-variation-default has-page-masthead-color has-color-primary-background-color has-text-color has-background has-link-color" style="border-top-color:var(--wp--preset--color--borders);border-top-width:1px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"},"blockGap":{"top":"0","left":"var:preset|spacing|50"}}},"backgroundColor":"footer-widgets-background","textColor":"footer-widgets","fontSize":"14"} -->
+	<div class="wp-block-columns alignfull has-footer-widgets-color has-footer-widgets-background-background-color has-text-color has-background has-14-font-size" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:paragraph {"fontSize":"18"} -->
-<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+			<!-- wp:paragraph {"fontSize":"18"} -->
+			<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
-<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:paragraph {"fontSize":"18"} -->
-<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+			<!-- wp:paragraph {"fontSize":"18"} -->
+			<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
-<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:paragraph {"fontSize":"18"} -->
-<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+			<!-- wp:paragraph {"fontSize":"18"} -->
+			<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
-<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Title of Footer Section</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:paragraph {"fontSize":"18"} -->
-<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
+			<!-- wp:paragraph {"fontSize":"18"} -->
+			<p class="has-18-font-size">Add your footer content here. Describe your membership community or add useful links.</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns -->
 
-<!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|borders","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|40","bottom":"var:preset|spacing|20","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"site-navigation-background","layout":{"type":"flex","justifyContent":"flex-start","flexWrap":"wrap"}} -->
-<div class="wp-block-group alignfull has-site-navigation-background-background-color has-background" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--40)"><!-- wp:navigation {"ref":2196,"textColor":"site-navigation-link","overlayBackgroundColor":"site-navigation-background","overlayTextColor":"site-navigation-link","style":{"typography":{"textDecoration":"underline"}}} /--></div>
-<!-- /wp:group -->
+	<!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|borders","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|40","bottom":"var:preset|spacing|20","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"site-navigation-background","layout":{"type":"flex","justifyContent":"flex-start","flexWrap":"wrap"}} -->
+	<div class="wp-block-group alignfull has-site-navigation-background-background-color has-background" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--40)"><!-- wp:navigation {"ref":2196,"textColor":"site-navigation-link","overlayBackgroundColor":"site-navigation-background","overlayTextColor":"site-navigation-link","style":{"typography":{"textDecoration":"underline"}}} /--></div>
+	<!-- /wp:group -->
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|40","bottom":"var:preset|spacing|20","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"fontSize":"18"} -->
-<p class="has-18-font-size">© 2026 Your Site Name</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|40","bottom":"var:preset|spacing|20","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"site-navigation-background","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap","verticalAlignment":"center"}} -->
+	<div class="wp-block-group alignfull has-site-navigation-background-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-link"}}}},"textColor":"site-navigation-link","fontSize":"18"} -->
+		<p class="has-site-navigation-link-color has-text-color has-link-color has-18-font-size">© 2026 Your Site Name</p>
+		<!-- /wp:paragraph --></div>
+	<!-- /wp:group --></div>
 <!-- /wp:group -->
