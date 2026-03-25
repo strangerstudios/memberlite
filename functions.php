83f9ffe6eebbd02e602311d9bd00624acf317893
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-03-25 09:01:41 -0400

    move the back to top function to the customizer.php file

--- functions.php
+++ functions.php
@@ -77,43 +77,6 @@ function memberlite_admin_enqueue_scripts() {
 }
 add_action( 'admin_enqueue_scripts', 'memberlite_admin_enqueue_scripts' );
 
-/**
- * Output the floating back-to-top button in wp_footer.
- * Visibility is controlled by JS (scroll position + IntersectionObserver on the footer).
- * (Turned on in Customizer > General Settings)
- *
- * @version TBD
- * @return void
- */
-function memberlite_floating_back_to_top() {
-	global $memberlite_defaults;
-
-	if ( empty( get_theme_mod( 'memberlite_back_to_top', $memberlite_defaults['memberlite_back_to_top'] ) ) ) {
-		return;
-	}
-
-	if ( 'floating' !== get_theme_mod( 'memberlite_back_to_top_style', $memberlite_defaults['memberlite_back_to_top_style'] ) ) {
-		return;
-	}
-
-	$label = apply_filters(
-		'memberlite_back_to_top',
-		'<i class="fa fa-chevron-up" aria-hidden="true"></i><span class="screen-reader-text">' . esc_html__( 'Back to Top', 'memberlite' ) . '</span>'
-	);
-
-	if ( empty( $label ) ) {
-		return;
-	}
-
-	$allowed_html = array(
-		'i'    => array( 'class' => array(), 'aria-hidden' => array() ),
-		'span' => array( 'class' => array() ),
-	);
-
-	echo '<a class="memberlite-back-to-top floating" href="#page">' . wp_kses( $label, $allowed_html ) . '</a>';
-}
-add_action( 'wp_footer', 'memberlite_floating_back_to_top' );
-
 function memberlite_get_font( $font_type, $nicename = NULL ) {
 	global $memberlite_defaults;
 
