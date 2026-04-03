e7e836e942747b09542e312f3b43f6b8104e2493
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-03 14:30:11 -0400

    new pattern for the footer using the icon block from WP 7.0

---
+++ patterns/footer-03.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Title: Footer Variation 3
+ * Slug: memberlite/footer-03
+ * Description: Footer with logo on the left and 3 columns on the right.
+ * Categories: memberlite-footer, footer
+ * Keywords: footer, variation
+ * @package WordPress
+ * @subpackage Memberlite
+ * @since TBD
+ */
+?>
+<!-- wp:group {"align":"full","className":"footer-variation-01","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-background"}}}},"backgroundColor":"color-primary","textColor":"site-navigation-background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull footer-variation-03 has-site-navigation-background-color has-color-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%"} -->
+		<div class="wp-block-column" style="flex-basis:33%"><!-- wp:site-logo {"width":242} /--></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+		<div class="wp-block-column"><!-- wp:columns -->
+			<div class="wp-block-columns"><!-- wp:column -->
+				<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"border":{"bottom":{"color":"var:preset|color|color-secondary","width":"1px"}},"spacing":{"padding":{"bottom":"5px"}}}} -->
+					<h3 class="wp-block-heading" style="border-bottom-color:var(--wp--preset--color--color-secondary);border-bottom-width:1px;padding-bottom:5px">Useful Links</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+						<p style="margin-top:0"><a href="#">Login</a></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+						<p style="margin-top:0"><a href="#">Membership Levels</a></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+						<p style="margin-top:0"><a href="#">Join Us</a></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+						<p style="margin-top:0"><a href="#">Our Mission</a></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:column -->
+
+				<!-- wp:column -->
+				<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"border":{"bottom":{"color":"var:preset|color|color-secondary","width":"1px"}},"spacing":{"padding":{"bottom":"5px"}}}} -->
+					<h3 class="wp-block-heading" style="border-bottom-color:var(--wp--preset--color--color-secondary);border-bottom-width:1px;padding-bottom:5px">Support</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}},"typography":{"textDecoration":"none"}}} -->
+						<p style="margin-top:0;text-decoration:none"><a href="mailto:#">info@email.com</a></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+						<p style="margin-top:0">(123) 456-7890</p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|10"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+					<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:icon {"icon":"core/chevron-right","style":{"elements":{"link":{"color":{"text":"var:preset|color|color-action"}}}},"textColor":"color-action"} /-->
+
+						<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}},"typography":{"textDecoration":"none"}}} -->
+						<p style="margin-top:0;text-decoration:none"><a href="#">Contact Us</a></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:column -->
+
+				<!-- wp:column -->
+				<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"border":{"bottom":{"color":"var:preset|color|color-secondary","width":"1px"}},"spacing":{"padding":{"bottom":"5px"}}}} -->
+					<h3 class="wp-block-heading" style="border-bottom-color:var(--wp--preset--color--color-secondary);border-bottom-width:1px;padding-bottom:5px">Follow Us!</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:social-links {"iconColor":"site-navigation-background","iconColorValue":"#f9fafb","size":"has-large-icon-size","className":"is-style-logos-only","style":{"spacing":{"margin":{"top":"var:preset|spacing|10"},"blockGap":{"left":"var:preset|spacing|10"}}}} -->
+					<ul class="wp-block-social-links has-large-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--10)"><!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+						<!-- wp:social-link {"url":"#","service":"bluesky"} /-->
+
+						<!-- wp:social-link {"url":"#","service":"x"} /-->
+
+						<!-- wp:social-link {"url":"#","service":"facebook"} /--></ul>
+					<!-- /wp:social-links --></div>
+				<!-- /wp:column --></div>
+			<!-- /wp:columns --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
