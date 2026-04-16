d4e25b14d2fb054aa3c36d3cacba2c0fbac98a5b
Kim Coleman <kim@strangerstudios.com>
2026-04-15 20:38:17 -0400

    Removing unnecessary code comment

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -582,7 +582,6 @@ function memberlite_get_legacy_color_scheme_definitions(): array {
  * @return string
  */
 function memberlite_get_variation( $slug ) {
-	// Trigger a deprecation notice
 	_deprecated_function( __FUNCTION__, '7.1' );
 
 	// No replacement for this function, returning 'default' will get the components/footer/variation-default.php template part
