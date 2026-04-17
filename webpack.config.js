eeaf3d63375861415c0f01ee9e0c2d3725b08aaa
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-17 13:22:41 -0400

    add new script for the block inserter icon

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require( 'path' );
 module.exports = {
 	...defaultConfig,
 	entry: {
+		'editor/block-inserter': path.resolve( process.cwd(), 'src/editor/block-inserter.js' ),
 		'editor/custom-settings': path.resolve( process.cwd(), 'src/editor/custom-settings.js' ),
 		'blocks/nav-menu/index': path.resolve( process.cwd(), 'src/blocks/nav-menu/index.js' ),
 		'blocks/member-info/index': path.resolve( process.cwd(), 'src/blocks/member-info/index.js' ),
