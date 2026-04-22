3193a75055d49247e55f3c82266ded00363b5fbb
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-22 16:03:07 -0400

    improved header-04

--- patterns/header-04.php
+++ patterns/header-04.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Title: Header 04
+ * Title: Header: Logo, Nav & CTA
  * Slug: memberlite/header-04
- * Description: Colored header bar with site logo on the left, a centered navigation, and a CTA button on the right.
+ * Description: Colored header bar with site logo on the left, centered navigation, and a CTA button on the right.
  * Categories: memberlite-header, header
  * Keywords: header, navigation, member, cta, call to action
  * Viewport Width: 1280
@@ -12,15 +12,21 @@
  */
 ?>
 <!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|borders","width":"1px"}},"elements":{"link":{"color":{"text":"var:preset|color|footer-widgets"}}},"spacing":{"padding":{"top":"0","bottom":"0"}}},"backgroundColor":"footer-widgets-background","textColor":"footer-widgets","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-footer-widgets-color has-footer-widgets-background-background-color has-text-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;padding-top:0;padding-bottom:0"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0","padding":{"right":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-right:0;padding-left:0"><!-- wp:site-title {"fontSize":"24"} /-->
+<div class="wp-block-group alignfull has-footer-widgets-color has-footer-widgets-background-background-color has-text-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;padding-top:0;padding-bottom:0"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"left":"0"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center" style="margin-top:0;margin-bottom:0"><!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:site-title {"fontSize":"24"} /--></div>
+<!-- /wp:column -->
 
-<!-- wp:memberlite/nav-menu {"fontSize":"16"} /-->
+<!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:memberlite/nav-menu {"fontSize":"16"} /--></div>
+<!-- /wp:column -->
 
-<!-- wp:buttons {"align":"wide","fontSize":"16","layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons alignwide has-custom-font-size has-16-font-size"><!-- wp:button {"backgroundColor":"color-action","fontSize":"16"} -->
+<!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"color-action","fontSize":"16"} -->
 <div class="wp-block-button"><a class="wp-block-button__link has-color-action-background-color has-background has-16-font-size has-custom-font-size wp-element-button">[fa icon="heart"] Join Today</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
-<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
