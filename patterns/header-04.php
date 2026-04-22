fe0b59d04fb5a2ae3b6af5fce52355ad20fa9751
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-22 15:23:23 -0400

    first attempt

---
+++ patterns/header-04.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Header 04
+ * Slug: memberlite/header-04
+ * Description: Colored header bar with site logo on the left, a centered navigation, and a CTA button on the right.
+ * Categories: memberlite-header, header
+ * Keywords: header, navigation, member, cta, call to action
+ * Viewport Width: 1280
+ * Block Types: core/post-content
+ * Post Types: memberlite_header
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|borders","width":"1px"}},"elements":{"link":{"color":{"text":"var:preset|color|footer-widgets"}}},"spacing":{"padding":{"top":"0","bottom":"0"}}},"backgroundColor":"footer-widgets-background","textColor":"footer-widgets","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-footer-widgets-color has-footer-widgets-background-background-color has-text-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;padding-top:0;padding-bottom:0"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0","padding":{"right":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-right:0;padding-left:0"><!-- wp:site-title {"fontSize":"24"} /-->
+
+<!-- wp:memberlite/nav-menu {"fontSize":"16"} /-->
+
+<!-- wp:buttons {"align":"wide","fontSize":"16","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignwide has-custom-font-size has-16-font-size"><!-- wp:button {"backgroundColor":"color-action","fontSize":"16"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-color-action-background-color has-background has-16-font-size has-custom-font-size wp-element-button">[fa icon="heart"] Join Today</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/header-05.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Title: Header 05
+ * Slug: memberlite/header-05
+ * Description: 
+ * Categories: 
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"color-primary","textColor":"white","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-white-color has-color-primary-background-color has-text-color has-background has-link-color"><!-- wp:paragraph -->
+<p>Some sale now!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:loginout {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"color-secondary"} /-->
+
+<!-- wp:social-links {"iconColor":"white","iconColorValue":"#ffffff","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"x"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|footer-widgets"}}}}} -->
+<p class="has-link-color">Home Shop About</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:site-logo /-->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|footer-widgets"}}}}} -->
+<p class="has-link-color">Services Blog Contact </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
