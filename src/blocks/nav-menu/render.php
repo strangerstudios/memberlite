c6335cc3b7557ad26a780b6f00b14c6703ee9f54
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-22 13:51:50 -0400

    realized the ID was being shared across multiple nav menu blocks so I added a class instead, updated any related styling/JS

--- src/blocks/nav-menu/render.php
+++ src/blocks/nav-menu/render.php
@@ -29,8 +29,9 @@ if ( ! empty( $attributes['backgroundColor'] ) ) {
 	$nav_style_vars = '--memberlite-color-navigation-block-background:' . esc_attr( $attributes['style']['color']['background'] ) . ';';
 }
 
+//@todo: Easiest fix I think is to apply a class and then add that class to the header.scss styles for site-navigation...
 $extra_attrs = array(
-	'id'         => 'site-navigation',
+	'class'      => 'memberlite-nav-menu',
 	'role'       => 'navigation',
 	'aria-label' => $aria_label,
 );
