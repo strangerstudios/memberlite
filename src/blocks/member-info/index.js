51c76db017f3fae4ef072f3da9354a9cb8876b30
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-23 11:24:42 -0400

    remove comments that come with creating blocks from wp-scripts

--- src/blocks/member-info/index.js
+++ src/blocks/member-info/index.js
@@ -1,25 +1,9 @@
-/**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
 import { registerBlockType } from '@wordpress/blocks';
 
-/**
- * Internal dependencies
- */
 import Edit from './edit';
 import metadata from './block.json';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
 registerBlockType( metadata.name, {
-	/**
-	 * @see ./edit.js
-	 */
 	icon: {
 		background: '#FFFFFF',
 		foreground: '#00a59d',
