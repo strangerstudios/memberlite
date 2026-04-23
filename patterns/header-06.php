9388db8c39d4805de3d9c3eb591351e0d49c2c98
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-23 14:01:03 -0400

    header 06, hero version

---
+++ patterns/header-06.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Title: Header: Header 06
+ * Slug: memberlite/header-06
+ * Description: A full-width hero header with a dark overlay image, top navigation bar, tagline, bold heading, event date, and call-to-action buttons. Ideal for conference or event membership sites.
+ * Categories: memberlite-header, header
+ * Keywords: header, navigation, logo, conference, event, hero
+ * Viewport Width: 1280
+ * Block Types: core/post-content
+ * Post Types: memberlite_header
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/patterns/images/landscapes/syed-hadi-naqvi-DzyrFjJbLMg-unsplash-md.jpg","dimRatio":80,"overlayColor":"site-navigation-link","isUserOverlayColor":true,"align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10","bottom":"var:preset|spacing|60","top":"0"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover alignwide" style="padding-top:0;padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--10)"><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/patterns/images/landscapes/syed-hadi-naqvi-DzyrFjJbLMg-unsplash-md.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-site-navigation-link-background-color has-background-dim-80 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"border":{"bottom":{"color":"var:preset|color|white","width":"1px"}},"spacing":{"padding":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+			<div class="wp-block-group alignwide has-white-color has-text-color has-link-color" style="border-bottom-color:var(--wp--preset--color--white);border-bottom-width:1px;padding-right:0;padding-left:0"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+				<div class="wp-block-group"><!-- wp:site-logo {"width":48} /-->
+
+					<!-- wp:site-title {"isLink":false,"fontSize":"18"} /--></div>
+				<!-- /wp:group -->
+
+				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right","verticalAlignment":"center"}} -->
+				<div class="wp-block-group"><!-- wp:memberlite/nav-menu {"selectedMenu":"menu:211","fontSize":"16"} /-->
+
+					<!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"center","justifyContent":"right"}} -->
+					<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"color-action","textColor":"site-navigation-link","style":{"spacing":{"padding":{"top":"5px","bottom":"5px"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-link"}}}},"fontSize":"16"} -->
+						<div class="wp-block-button"><a class="wp-block-button__link has-site-navigation-link-color has-color-action-background-color has-text-color has-background has-link-color has-16-font-size has-custom-font-size wp-element-button" style="padding-top:5px;padding-bottom:5px">Register Now!</a></div>
+						<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:columns -->
+			<div class="wp-block-columns"><!-- wp:column {"width":"70%"} -->
+				<div class="wp-block-column" style="flex-basis:70%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph -->
+						<p>The Annual Membership Summit</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"42"} -->
+						<h2 class="wp-block-heading has-42-font-size" style="margin-top:5px">Everything You Need to Launch, Grow, and Scale Your Membership</h2>
+						<!-- /wp:heading --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group" style="margin-top:0"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#ffd100"}}},"spacing":{"margin":{"top":"var:preset|spacing|10"}},"color":{"text":"#ffd100"}},"fontSize":"21"} -->
+						<p class="has-text-color has-link-color has-21-font-size" style="color:#ffd100;margin-top:var(--wp--preset--spacing--10)"><strong>November 10–14, 2026</strong></p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"center","justifyContent":"left"}} -->
+						<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"color-action","textColor":"site-navigation-link","className":"is-style-fill","style":{"spacing":{"padding":{"top":"5px","bottom":"5px"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-link"}}}},"fontSize":"21","borderColor":"white"} -->
+							<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-site-navigation-link-color has-color-action-background-color has-text-color has-background has-link-color has-border-color has-white-border-color has-21-font-size has-custom-font-size wp-element-button" style="padding-top:5px;padding-bottom:5px">Learn More [fa icon="angle-right"]</a></div>
+							<!-- /wp:button -->
+
+							<!-- wp:button {"backgroundColor":"color-action","textColor":"site-navigation-link","style":{"spacing":{"padding":{"top":"5px","bottom":"5px"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-link"}}},"border":{"width":"0px","style":"none"}},"fontSize":"21"} -->
+							<div class="wp-block-button"><a class="wp-block-button__link has-site-navigation-link-color has-color-action-background-color has-text-color has-background has-link-color has-21-font-size has-custom-font-size wp-element-button" style="border-style:none;border-width:0px;padding-top:5px;padding-bottom:5px">Register Now [fa icon="angle-right"]</a></div>
+							<!-- /wp:button --></div>
+						<!-- /wp:buttons --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:column -->
+
+				<!-- wp:column {"width":"30%"} -->
+				<div class="wp-block-column" style="flex-basis:30%"></div>
+				<!-- /wp:column --></div>
+			<!-- /wp:columns --></div></div>
+	<!-- /wp:cover --></div>
+<!-- /wp:group -->
