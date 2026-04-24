975814027ebdf86ca728fdb40ce6e285a48fcb5f
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-24 15:33:39 -0400

    Removed block category for memberlite, added block collection instead (still looks the same in the block inserter except it automatically grabs our blocks by theme slug), and updated the block.json files for our blocks to also appear in the core 'theme' section

--- src/editor/block-inserter.js
+++ src/editor/block-inserter.js
@@ -7,6 +7,12 @@ import domReady from '@wordpress/dom-ready';
 import { updateCategory } from '@wordpress/blocks';
 
 domReady( () => {
+	wp.blocks.registerBlockCollection( 'memberlite', {
+		title: 'Memberlite',
+		slug: 'memberlite',
+		icon: PMProIcon,
+	});
+
 	updateCategory( 'memberlite', {
 		icon: PMProIcon
 	} );
