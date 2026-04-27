ebd64bd9c5f61a7cb80f4bb20ec2f7c751e0668a
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-04-27 14:19:53 -0400

    put SVG directly into edit links for better color control

--- components/page/content-page.php
+++ components/page/content-page.php
@@ -28,7 +28,9 @@
 	</div><!-- .entry-content -->
 	<?php if ( current_user_can( 'edit_post', get_the_ID() ) ) { ?>
 		<footer class="entry-footer">
-			<?php edit_post_link( esc_html__( 'Edit', 'memberlite' ), '<span class="edit-link">', '</span>' ); ?>
+			<?php edit_post_link( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+	<path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"></path>
+	</svg>'. esc_html__( 'Edit Page', 'memberlite' ), '<span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-footer -->
 	<?php } ?>
 </article><!-- #post-## -->
--- components/post/entry-footer.php
+++ components/post/entry-footer.php
@@ -18,6 +18,8 @@ if ( ! empty( $memberlite_get_entry_meta_after ) || current_user_can( 'edit_post
 			echo Memberlite_Customize::sanitize_text_with_links( $memberlite_get_entry_meta_after ); // WPCS: xss ok.
 		}
 		?>
-		<?php edit_post_link( esc_html__( 'Edit', 'memberlite' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+	<path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"></path>
+	</svg>'. esc_html__( 'Edit Post', 'memberlite' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 <?php } ?>
