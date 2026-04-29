30f0fad7097198e47655c82ef5f5470aa59ffa7e
Kim Coleman <kim@strangerstudios.com>
2026-04-29 11:32:20 -0400

    Nav menu block added; using member info instead of the CTA

--- patterns/header-04.php
+++ patterns/header-04.php
@@ -1,52 +1,26 @@
 <?php
 /**
- * Title: Header: Logo, Nav & CTA
+ * Title: Header: Logo, Nav & Member Info
  * Slug: memberlite/header-04
- * Description: Colored header bar with site logo on the left, centered navigation, and a CTA button on the right.
+ * Description: Header bar with site title and navigation on the left, and member info on the right.
  * Categories: memberlite-header, header
- * Keywords: header, navigation, member, cta, call to action
+ * Keywords: header, navigation, member, member info
  * Viewport Width: 1280
  * Block Types: core/post-content
  * Post Types: memberlite_header
  * Inserter: true
  */
 ?>
-<!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|borders","width":"1px"}},"elements":{"link":{"color":{"text":"var:preset|color|footer-widgets"}}},"spacing":{"padding":{"top":"0","bottom":"0"}}},"backgroundColor":"footer-widgets-background","textColor":"footer-widgets","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-footer-widgets-color has-footer-widgets-background-background-color has-text-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;padding-top:0;padding-bottom:0"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"left":"0"},"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)"><!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:site-title {"fontSize":"24"} /--></div>
-<!-- /wp:column -->
+<!-- wp:group {"align":"full","className":"header-variation-04","style":{"border":{"bottom":{"color":"var:preset|color|borders","width":"1px"}},"elements":{"link":{"color":{"text":"var:preset|color|page-masthead"}}},"spacing":{"padding":{"top":"0","bottom":"0"}}},"backgroundColor":"page-masthead-background","textColor":"page-masthead","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull header-variation-04 has-page-masthead-color has-page-masthead-background-background-color has-text-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--borders);border-bottom-width:1px;padding-top:0;padding-bottom:0"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|page-masthead"}}}},"backgroundColor":"page-masthead-background","textColor":"page-masthead","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide has-page-masthead-color has-page-masthead-background-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--10);padding-right:0;padding-bottom:var(--wp--preset--spacing--10);padding-left:0"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:site-title {"fontSize":"24"} /-->
 
-<!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"16"} -->
-<p class="has-16-font-size"><a href="#">Home</a></p>
-<!-- /wp:paragraph -->
+<!-- wp:memberlite/nav-menu {"backgroundColor":"page-masthead-background","fontSize":"16"} /--></div>
+<!-- /wp:group -->
 
-<!-- wp:paragraph {"fontSize":"16"} -->
-<p class="has-16-font-size"><a href="#">About</a></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"fontSize":"16"} -->
-<p class="has-16-font-size"><a href="#">Membership</a></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"fontSize":"16"} -->
-<p class="has-16-font-size"><a href="#">Blog</a></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"fontSize":"16"} -->
-<p class="has-16-font-size"><a href="#">Contact</a></p>
-<!-- /wp:paragraph --></div>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}},"border":{"radius":{"topLeft":"20px","topRight":"20px","bottomLeft":"20px","bottomRight":"20px"}}},"backgroundColor":"color-secondary","fontSize":"16","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-color-secondary-background-color has-background has-16-font-size" style="border-top-left-radius:20px;border-top-right-radius:20px;border-bottom-left-radius:20px;border-bottom-right-radius:20px;padding-top:0;padding-right:var(--wp--preset--spacing--10);padding-bottom:0;padding-left:var(--wp--preset--spacing--10)"><!-- wp:memberlite/member-info /--></div>
 <!-- /wp:group --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"color-action","fontSize":"16"} -->
-<div class="wp-block-button"><a class="wp-block-button__link has-color-action-background-color has-background has-16-font-size has-custom-font-size wp-element-button">[fa icon="heart"] Join Today</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
