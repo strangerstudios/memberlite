c7a84e3274630f6ec33eada4e3c1a40d2430e1d7
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-07-14 11:13:35 -0400

    new block styles for several blocks registered

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -14,19 +14,60 @@
  * @return void
  */
 function memberlite_register_block_styles(): void {
-	register_block_style(
-		'core/list',
-		array(
-			'name'         => 'plain',
-			'label'        => __( 'Plain', 'memberlite' ),
-			'inline_style' => '
-				.wp-block-list.is-style-plain {
-					list-style: none;
-					padding-left: 0;
-					margin-left: 0;
-				}
-			',
-		)
+	$block_styles = array(
+		'core/list'      => array(
+			array( 'name' => 'plain', 'label' => __( 'Plain', 'memberlite' ) ),
+			array( 'name' => 'horizontal-left', 'label' => __( 'Horizontal Left', 'memberlite' ) ),
+			array( 'name' => 'horizontal-center', 'label' => __( 'Horizontal Center', 'memberlite' ) ),
+		),
+		'core/button'    => array(
+			//@todo: Consider adding a global button style option in Customizer for pill/sharp instead
+			//array( 'name' => 'pill', 'label' => __( 'Pill', 'memberlite' ) ),
+			//array( 'name' => 'sharp', 'label' => __( 'Sharp', 'memberlite' ) ),
+			array( 'name' => 'arrow-fill', 'label' => __( 'Arrow Fill', 'memberlite' ) ),
+			array( 'name' => 'arrow-plain', 'label' => __( 'Arrow Plain', 'memberlite' ) ),
+		),
+		'core/separator' => array(
+			array( 'name' => 'faded-edges', 'label' => __( 'Faded Edges', 'memberlite' ) ),
+			array( 'name' => 'double', 'label' => __( 'Double', 'memberlite' ) ),
+			array( 'name' => 'wave', 'label' => __( 'Wave', 'memberlite' ) ),
+			array( 'name' => 'flourish-diamond', 'label' => __( 'Diamond', 'memberlite' ) ),
+			array( 'name' => 'flourish-arrow', 'label' => __( 'Arrow', 'memberlite' ) ),
+			array( 'name' => 'flourish-triple-diamond', 'label' => __( 'Triple Diamond', 'memberlite' ) ),
+			array( 'name' => 'flourish-circle-diamond', 'label' => __( 'Circle Diamond', 'memberlite' ) ),
+		),
+		'core/search'    => array(
+			array( 'name' => 'filled-pill', 'label' => __( 'Filled Pill', 'memberlite' ) ),
+			array( 'name' => 'filled-sharp', 'label' => __( 'Filled Sharp', 'memberlite' ) ),
+			array( 'name' => 'icon-round', 'label' => __( 'Icon Round', 'memberlite' ) ),
+			array( 'name' => 'icon-square', 'label' => __( 'Icon Square', 'memberlite' ) ),
+		),
+		'core/loginout'  => array(
+			array( 'name' => 'loginout-button', 'label' => __( 'Button', 'memberlite' ) ),
+		),
+		'core/cover'     => array(
+			array( 'name' => 'scroll-indicator', 'label' => __( 'Scroll Indicator', 'memberlite' ) ),
+			array( 'name' => 'slant-bottom-right', 'label' => __( 'Slant: Bottom Right', 'memberlite' ) ),
+			array( 'name' => 'slant-bottom-left', 'label' => __( 'Slant: Bottom Left', 'memberlite' ) ),
+			array( 'name' => 'wavy-bottom', 'label' => __( 'Wavy Bottom', 'memberlite' ) ),
+		),
+		'core/accordion'  => array(
+			array( 'name' => 'accrdn-medium', 'label' => __( 'Medium Plus', 'memberlite' ) ),
+			array( 'name' => 'accrdn-large', 'label' => __( 'Large Plus', 'memberlite' ) ),
+			array( 'name' => 'accrdn-caret', 'label' => __( 'Caret', 'memberlite' ) ),
+		),
+		'core/heading' => array(
+			array(
+				'name'  => 'heading-rule',
+				'label' => __( 'Rule', 'loresmith' )
+			),
+		),
 	);
+
+	foreach ( $block_styles as $block_name => $styles ) {
+		foreach ( $styles as $style ) {
+			register_block_style( $block_name, $style );
+		}
+	}
 }
 add_action( 'init', 'memberlite_register_block_styles' );
