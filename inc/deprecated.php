3249ea46fdbea604384cac99d5455d718d0eef84
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-07-22 11:31:52 -0400

    tweaks from co-pilot feedback

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -598,7 +598,7 @@ function memberlite_get_variation( $slug ) {
 function memberlite_enqueue_block_assets() {
 	_doing_it_wrong(
 		__FUNCTION__,
-		esc_html__( 'Editor styles are now registered via add_editor_style() in memberlite_setup().' , 'memberlite' ),
+		esc_html__( 'Editor styles are now registered via add_editor_style() in memberlite_setup().', 'memberlite' ),
 		'TBD'
 	);
 }
