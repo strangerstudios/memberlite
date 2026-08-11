e485b6a45df8600351269c6ac231a35d0b7f62c5
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-08-11 15:43:21 -0400

    formatting adjustments

--- js/admin-page_banners.js
+++ js/admin-page_banners.js
@@ -27,7 +27,7 @@ if (wp.data && window.tinymce) {
 		// the post is currently being saved && we have tinymce editors
 		if (wp.data.select( 'core/editor' ).isSavingPost() && window.tinymce.editors) {
 			for (var i = 0; i < tinymce.editors.length; i++) {
-				tinymce.editors[i].save();
+			tinymce.editors[i].save();
 			}
 		}
 	});
