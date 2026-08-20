3cec9e2547cc1fdb72cb96c4e3501392e345f68b
RachelRVasquez <rachelrvasquez@gmail.com>
2026-08-20 14:30:17 -0400

    more adjustments to patterns w/ accordions

--- patterns/accordion-content-with-cta.php
+++ patterns/accordion-content-with-cta.php
@@ -35,48 +35,48 @@
 		<!-- wp:column -->
 		<div class="wp-block-column"><!-- wp:accordion {"className":"is-style-accrdn-large"} -->
 			<div role="group" class="wp-block-accordion is-style-accrdn-large"><!-- wp:accordion-item {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-background"}}}},"backgroundColor":"site-navigation-link","textColor":"site-navigation-background"} -->
-				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"spacing":{"padding":{"top":"6px","bottom":"6px","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}},"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"}},"fontSize":"24"} -->
-					<h3 class="wp-block-accordion-heading has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-top:6px;padding-right:var(--wp--preset--spacing--10);padding-bottom:6px;padding-left:var(--wp--preset--spacing--10)"><span class="wp-block-accordion-heading__toggle-title">How much does membership cost?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
+				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"},"spacing":{"padding":{"left":"0"}}},"fontSize":"24"} -->
+					<h3 class="wp-block-accordion-heading has-icon has-icon-right has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-left:0"><span class="wp-block-accordion-heading__toggle-title">How much does membership cost?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
 					<!-- /wp:accordion-heading -->
 
-					<!-- wp:accordion-panel {"style":{"spacing":{"padding":{"top":"0"}}}} -->
-					<div role="region" class="wp-block-accordion-panel" style="padding-top:0"><!-- wp:paragraph -->
+					<!-- wp:accordion-panel -->
+					<div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
 						<p>We offer monthly and annual plans to fit your budget, with a discount for paying yearly. You can view full pricing on our membership levels page.</p>
 						<!-- /wp:paragraph --></div>
 					<!-- /wp:accordion-panel --></div>
 				<!-- /wp:accordion-item -->
 
 				<!-- wp:accordion-item {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-background"}}}},"backgroundColor":"site-navigation-link","textColor":"site-navigation-background"} -->
-				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"spacing":{"padding":{"top":"6px","bottom":"6px","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}},"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"}},"fontSize":"24"} -->
-					<h3 class="wp-block-accordion-heading has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-top:6px;padding-right:var(--wp--preset--spacing--10);padding-bottom:6px;padding-left:var(--wp--preset--spacing--10)"><span class="wp-block-accordion-heading__toggle-title">Do you offer a free trial?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
+				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"},"spacing":{"padding":{"left":"0"}}},"fontSize":"24"} -->
+					<h3 class="wp-block-accordion-heading has-icon has-icon-right has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-left:0"><span class="wp-block-accordion-heading__toggle-title">Do you offer a free trial?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
 					<!-- /wp:accordion-heading -->
 
-					<!-- wp:accordion-panel {"style":{"spacing":{"padding":{"top":"0"}}}} -->
-					<div role="region" class="wp-block-accordion-panel" style="padding-top:0"><!-- wp:paragraph -->
+					<!-- wp:accordion-panel -->
+					<div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
 						<p>Yes, new members get a 7-day free trial with full access to member-only content before your card is ever charged.</p>
 						<!-- /wp:paragraph --></div>
 					<!-- /wp:accordion-panel --></div>
 				<!-- /wp:accordion-item -->
 
 				<!-- wp:accordion-item {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-background"}}}},"backgroundColor":"site-navigation-link","textColor":"site-navigation-background"} -->
-				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"spacing":{"padding":{"top":"6px","bottom":"6px","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}},"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"}},"fontSize":"24"} -->
-					<h3 class="wp-block-accordion-heading has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-top:6px;padding-right:var(--wp--preset--spacing--10);padding-bottom:6px;padding-left:var(--wp--preset--spacing--10)"><span class="wp-block-accordion-heading__toggle-title">Can I upgrade or downgrade my plan?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
+				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"},"spacing":{"padding":{"left":"0"}}},"fontSize":"24"} -->
+					<h3 class="wp-block-accordion-heading has-icon has-icon-right has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-left:0"><span class="wp-block-accordion-heading__toggle-title">Can I upgrade or downgrade my plan?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
 					<!-- /wp:accordion-heading -->
 
-					<!-- wp:accordion-panel {"style":{"spacing":{"padding":{"top":"0"}}}} -->
-					<div role="region" class="wp-block-accordion-panel" style="padding-top:0"><!-- wp:paragraph -->
+					<!-- wp:accordion-panel -->
+					<div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
 						<p>Absolutely. You can change your membership level any time from your account dashboard, and your billing will adjust automatically.</p>
 						<!-- /wp:paragraph --></div>
 					<!-- /wp:accordion-panel --></div>
 				<!-- /wp:accordion-item -->
 
 				<!-- wp:accordion-item {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}},"elements":{"link":{"color":{"text":"var:preset|color|site-navigation-background"}}}},"backgroundColor":"site-navigation-link","textColor":"site-navigation-background"} -->
-				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"spacing":{"padding":{"top":"6px","bottom":"6px","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}},"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"}},"fontSize":"24"} -->
-					<h3 class="wp-block-accordion-heading has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-top:6px;padding-right:var(--wp--preset--spacing--10);padding-bottom:6px;padding-left:var(--wp--preset--spacing--10)"><span class="wp-block-accordion-heading__toggle-title">What if I'm not happy with my membership?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
+				<div class="wp-block-accordion-item has-site-navigation-background-color has-site-navigation-link-background-color has-text-color has-background has-link-color" style="margin-bottom:var(--wp--preset--spacing--10)"><!-- wp:accordion-heading {"style":{"border":{"width":"0px","style":"none"},"typography":{"textTransform":"capitalize"},"spacing":{"padding":{"left":"0"}}},"fontSize":"24"} -->
+					<h3 class="wp-block-accordion-heading has-icon has-icon-right has-24-font-size" style="border-style:none;border-width:0px;text-transform:capitalize"><button type="button" class="wp-block-accordion-heading__toggle" style="padding-left:0"><span class="wp-block-accordion-heading__toggle-title">What if I'm not happy with my membership?</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
 					<!-- /wp:accordion-heading -->
 
-					<!-- wp:accordion-panel {"style":{"spacing":{"padding":{"top":"0"}}}} -->
-					<div role="region" class="wp-block-accordion-panel" style="padding-top:0"><!-- wp:paragraph -->
+					<!-- wp:accordion-panel -->
+					<div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
 						<p>We offer a 30-day money-back guarantee. If membership isn't the right fit, just reach out and we'll take care of a refund.</p>
 						<!-- /wp:paragraph --></div>
 					<!-- /wp:accordion-panel --></div>
